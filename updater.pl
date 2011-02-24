03f73c4bf6c1c5efd1bf0e8ef57a45579674b1a8
Stephen <sgsmith1@memphis.edu>
2011-02-24 16:13:37 -0600

    Added memory usage sub and some debugging statements for a logfile to print out. The current working environment experiences seemingly random coredumps running on FreeBSD-8.1 RELEASE and Perl 5.12.3

--- updater.pl
+++ updater.pl
@@ -6,6 +6,7 @@
 use strict;
 use Time::Local;
 use DateTime;
+use Proc::ProcessTable;
 
 #globals
 our $BGPDUMP = "bgpdump";
@@ -19,6 +20,7 @@ our $STOP_MINUTE = "";
 our $STOP_SECOND = "";
 #locals
 my $argnum = "";
+our $logfile = "arcturus.log";
 
 foreach $argnum (0..$#ARGV){
 	if($ARGV[$argnum] eq "-d"){
@@ -41,15 +43,15 @@ foreach $argnum (0..$#ARGV){
 		$BGPDUMP = $ARGV[$argnum + 1];
 	}	
 }
-sleep(10);
+
 our $stop_time = 0;	
-#hash for the stop times to be read in from the final, official rib table dump
-our %tabledump = ();
 $stop_time = getLastSecond(convert($YEAR.$MONTH.$DAY));
-
+####Debugging####
+open( logfile, ">>$logfile" ) || die("Cannot Open arcturus.log File");
+print logfile "$YEAR/$MONTH/$DAY|";
+close( logfile );
+#################
 applyUpdates($DIRECTORY);
-
-sleep(5);
 
 sub getLastSecond{
 	#This function is used to get the last update before the stop time.
@@ -81,7 +83,7 @@ sub getDateTime{
 
 sub print_help{
 	print "\nOptions:";
-	print "BGP Table Updater - Version 1.6.3\n";
+	print "BGP Table Updater - Version 1.6.4\n";
 	print "\n-h [help]      :\tThis screen";
 	print "\n-d [directory] :\tMain directory where all of the tables are kept.\n\t\t\tbgpdump should be placed here.";
 	print "\n-t [timestamp] :\tThe time (inclusive) in HH MM to apply the update ";
@@ -113,17 +115,23 @@ sub stripUpdate {
 	return ( $_[0] );
 }
 sub applyUpdates {
-
+        
 	#the update directory as given from the main subroutine and open it
 	my $updateDir  = $_[0];
 	opendir( dir_temp, $updateDir );
-
+	
 
 #Need to get the rib file from the directory just opened and give it the full path
 	my @rib     = grep( /rib/, readdir(dir_temp) );
 	closedir( dir_temp );
 	my $ribfile = $rib[0];
 	$ribfile = $updateDir . "/" . $ribfile;
+
+	####Debugging####
+	open( logfile, ">>$logfile" ) || die("Cannot Open arcturus.log File");
+	print logfile "$ribfile|";
+	close( logfile );
+	#################
 
 	#Do the same with all of the updates, but storing them in an array
 	opendir( dir_temp, $updateDir );
@@ -135,14 +143,14 @@ sub applyUpdates {
 	my $ribcommand_base    = "./".$BGPDUMP." -m -t dump ";
 	my $updatecommand_base = "./".$BGPDUMP." -m ";
 	my $ribcommand         = $ribcommand_base . $ribfile . " |";
-
+	
 
 	#open the rib file and pipe the output to this script
 	open( BGPDUMP, $ribcommand ) || die "Failed at " . $ribcommand . "!\n";
 
 	#Loop through the output's lines
 	#Hash array for storing the output from the table dump file
-	%tabledump = ();
+	my %tabledump = ();
 	while (<BGPDUMP>) {
 
 		#for each line, split on the "|" delimeter and store it in an array
@@ -163,10 +171,15 @@ sub applyUpdates {
 	close( BGPDUMP );
 	#All of the updates in the array are processed here
 	foreach (@updates) {
-
 		#Open the update and create the update command
 		my $updatefile    = $updateDir . "/" . $_;
 		my $updatecommand = $updatecommand_base . $updatefile . " |";
+
+		####Debugging####
+		open( logfile, ">>$logfile" ) || die("Cannot Open arcturus.log File");
+		print logfile "$updatefile|";
+		close( logfile );
+		#################
 
 		#Pipe the output and capture
 		open( UPDATE, $updatecommand ) || die "Failed at " . $updatecommand . "!\n";
@@ -213,7 +226,13 @@ sub applyUpdates {
 		}
 	undef @rib;
 	undef @updates;
-
+	
+	####Debugging####
+	open( logfile, ">>$logfile" ) || die("Cannot Open arcturus.log File");
+	print logfile  memory_usage(), "|";
+	close( logfile );
+	#################
+	
 	my $dt = getDateTime($stop_time);
 	my $outfile = $updateDir . "/" . "rib.".$dt.".0000.update.gz";
 	open(OUTPUTFILE,"|gzip -1 >$outfile");
@@ -226,12 +245,26 @@ sub applyUpdates {
 	}
 	undef %tabledump;
 	close(OUTPUTFILE);
+
+	####Debugging####
+	open( logfile, ">>$logfile" ) || die("Cannot Open arcturus.log File");
+	print logfile  "SUCCESS";
+	close( logfile );
+	#################
 }
 
 sub convert{
 	my @date = (substr($_[0],0,4), substr($_[0],4,2), substr($_[0],6,2));
 
 	return @date;
+}
+
+sub memory_usage {
+  my $t = new Proc::ProcessTable;
+  foreach my $got ( @{$t->table} ) {
+    next if not $got->pid eq $$;
+    return $got->size;
+  }
 }
 
 
